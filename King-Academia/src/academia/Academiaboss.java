ce2e82a854afa7d5ad455c88ffd2644c9339d7da
Rafael Paiva <rafaelpaiva062@gmail.com>
2024-11-18 20:24:55 -0300

    new

--- King-Academia/src/academia/Academiaboss.java
+++ King-Academia/src/academia/Academiaboss.java
@@ -1,7 +1,12 @@
 package academia;
 
 public class Academiaboss {
+<<<<<<< Updated upstream
     public static void main(String[] args) {
+=======
+    public static void mein(String [] args)
+    {
+>>>>>>> Stashed changes
         
     }
 }
